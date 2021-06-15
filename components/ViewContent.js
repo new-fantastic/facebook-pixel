73555087cc139c0028237d71197bc8dd9362a2ee
ibockowsky <igor@newfantastic.com>
2021-06-15 10:56:33 +0200

    fix: implement custom naming of product object

--- components/ViewContent.js
+++ components/ViewContent.js
@@ -1,9 +1,9 @@
 import prepareProductObject from '../util/prepareProductObject';
 import { isServer } from '@vue-storefront/core/helpers';
 
-export default {
+export default (productFieldToWatch) => ({
   watch: {
-    product: {
+    [productFieldToWatch]: {
       deep: true,
       immediate: true,
       handler (product, oldProduct) {
@@ -21,8 +21,8 @@ export default {
     }
   },
   methods: {
-    fbViewContent (product = this.product) {
+    fbViewContent (product = this[productFieldToWatch]) {
       window.fbq('track', 'ViewContent', prepareProductObject(product));
     }
   }
-};
+});
